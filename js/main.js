4b41414a97ff7dbec6104c58f67c6bd146863107
MrwanEslam <100796630+MrwanEslam@users.noreply.github.com>
2022-03-06 08:49:17 -0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// var cartona = ``;
+// for (var i = 1; i <= 100; i++) {
+//     cartona += `<div class="col-lg-4">
+//     <div class="item">
+//         <img src="2.jpg" class="w-100" alt="">
+//         <h1>Ahmed Ali</h1>
+//         <p>Lorem, ipsum dolor.</p>
+//     </div>
+// </div>`;
+// };
+// document.getElementById("myRow").innerHTML=cartona;
+
+
